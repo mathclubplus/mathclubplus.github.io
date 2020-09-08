750da86df54bf5253e1f11c777cf6dea5b84c3ab
bop34 <68297036+bop34@users.noreply.github.com>
2020-09-08 15:44:39 -0400

    Create loadGame.js

---
+++ js/loadGame.js
@@ -0,0 +1,8 @@
+htmlHeader = document.getElementById('header')
+htmlHeader.innerHTML = gName
+
+htmlGame = document.getElementById('embedGame')
+htmlGame.innerHTML = gCode
+
+htmlTitle = document.getElementById('title')
+htmlTitle.innerHTML = 'MathClub+ - ' + gName
