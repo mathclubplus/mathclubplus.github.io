a3ed156c1a7d2769eadbcaa09942352faf901022
bop34 <68297036+bop34@users.noreply.github.com>
2020-10-09 15:56:20 -0400

    Linked Penguin Problems 2 and redid other pages

--- js/loadAd.js
+++
@@ -1,8 +0,0 @@
-atOptions = {
-		'key' : '1d499dbc8b5376c4516cc0d44d765f5b',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pestilenttidefilth.org/1d499dbc8b5376c4516cc0d44d765f5b/invoke.js"></scr' + 'ipt>');
